87e1e8dafc0df44b27c0f4c572a46f2f1272c626
saddadnabbil <saddadnabbil>
2023-12-11 23:00:28 +0700

    fix: update logo for icon gis

--- resources/views/admin/silabus/pdf/view.blade.php
+++ resources/views/admin/silabus/pdf/view.blade.php
@@ -6,8 +6,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" sizes="16x16" href="/assets/dist/img/favicon.png">
+
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/dist/img/logo.png')}}">
+
     <title>{{$title}} | {{ env('APP_NAME') }} </title>
+    
     <style>
         body, html {
             margin: 0;
