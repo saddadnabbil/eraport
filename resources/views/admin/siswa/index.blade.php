aa5f243d076f666d9da765af8514b3c239167dff
saddadnabbil <saddadnabbil>
2023-12-13 09:02:28 +0700

    style: make col-md-3 in kelas

--- resources/views/admin/siswa/index.blade.php
+++ resources/views/admin/siswa/index.blade.php
@@ -26,7 +26,7 @@
     <div class="container-fluid">
             <!-- Info boxes -->
             <div class="row">
-              <div class="col-12 col-sm-6 col-md-2">
+              <div class="col-12 col-sm-6 col-md-3">
                 <div class="info-box">
                   <span class="info-box-icon bg-info elevation-1"><i class="fas fa-layer-group"></i></span>
       
@@ -39,7 +39,7 @@
                 <!-- /.info-box -->
               </div>
               <!-- /.col -->
-              <div class="col-12 col-sm-6 col-md-2">
+              <div class="col-12 col-sm-6 col-md-3">
                 <div class="info-box mb-3">
                   <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-book"></i></span>
       
@@ -56,7 +56,7 @@
               <!-- fix for small devices only -->
               <div class="clearfix hidden-md-up"></div>
       
-              <div class="col-12 col-sm-6 col-md-2">
+              <div class="col-12 col-sm-6 col-md-3">
                 <div class="info-box mb-3">
                   <span class="info-box-icon bg-success elevation-1"><i class="fas fa-users"></i></span>
       
@@ -69,7 +69,7 @@
                 <!-- /.info-box -->
               </div>
               <!-- /.col -->
-              <div class="col-12 col-sm-6 col-md-2">
+              <div class="col-12 col-sm-6 col-md-3">
                 <div class="info-box mb-3">
                   <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-book-reader "></i></span>
       
