011302d2ef7d27d8444f231d16b7c63ad3a9f9d3
saddadnabbil <saddadnabbil>
2024-05-16 07:45:58 +0700

    fix: select2

--- resources/views/layouts/main/header.blade.php
+++ resources/views/layouts/main/header.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/dist/img/logo.png') }}" />
     <title>{{ $title }} | {{ env('APP_NAME') }}</title>
@@ -17,25 +18,15 @@
     <link rel="stylesheet" href="{{ asset('assets/libs/fontawesome-free/css/all.min.css') }}">
     <!-- Bootstrap4 Duallistbox -->
     <link rel="stylesheet" href="{{ asset('assets/libs/bootstrap4-duallistbox/bootstrap-duallistbox.min.css') }}">
-
     {{-- Select2 --}}
     <link href="{{ asset('assets/libs/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/libs/select2/css/select2.min.css') }}" rel="stylesheet" />
     {{-- <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" /> --}}
-
     {{-- datatables --}}
-    <!-- <link href="../assets/extra-libs/datatables.net-bs4/css/dataTables.bootstrap4.css" rel="stylesheet"> -->
     <link rel="stylesheet" href="{{ asset('assets/extra-libs/datatables.net-bs4/css/dataTables.bootstrap4.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/extra-libs/datatables.net-bs4/css/responsive.dataTables.min.css') }}">
-
-    {{-- Style CSS untuk halaman ini --}}
+    {{-- Style CSS --}}
     @yield('styles')
-
-    <!-- HTML5 Shim dan Respond.js untuk mendukung elemen HTML5 dan kueri media di IE8 -->
-    <!-- PERINGATAN: Respond.js tidak akan berfungsi jika Anda melihat halaman melalui file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-        <![endif]-->
 </head>
 
 <body>
