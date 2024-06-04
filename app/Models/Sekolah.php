a5994259e6953ec191008a174caf9eae0802f460
Saddad Nabbil <saddadnabbil@gmail.com>
2024-06-04 07:40:11 +0700

    fix: bug get index array in count total student

--- app/Models/Sekolah.php
+++ app/Models/Sekolah.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Sekolah extends Model
 {
     use \Illuminate\Database\Eloquent\SoftDeletes;
-    
+
     protected $table = 'sekolah';
     protected $fillable = [
         'nama_sekolah',
@@ -29,17 +29,6 @@ class Sekolah extends Model
         return $this->belongsTo(Tapel::class, 'tapel_id');
     }
 
-    // public function semester()
-    // {
-    //     return $this->belongsTo(Semester::class, 'semester_id');
-    // }
-
-    // public function term()
-    // {
-    //     return $this->belongsTo(Term::class, 'term_id');
-    // }
-
-    // Contoh metode untuk mendapatkan tapel_id
     public function getTapelIdAttribute()
     {
         return $this->attributes['tapel_id'];
